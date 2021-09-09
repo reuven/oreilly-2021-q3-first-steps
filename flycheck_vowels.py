cfb67ee6c8152c911bddd9f07d804b2d5bed59a2
Reuven Lerner <reuven@lerner.co.il>
2021-09-09 21:31:11 +0300

    gitautosync update 66

---
+++ flycheck_vowels.py
@@ -0,0 +1,7 @@
+def count_vowels(filename):
+    count = 0
+
+    for one_line in open(filename):
+        for one_character in one_line.lower():
+            if one_character in 'aeiou':
+                
