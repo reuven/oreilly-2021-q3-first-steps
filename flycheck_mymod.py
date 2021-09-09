f108208e83205b7b7d2609aa1313b7e7bcadff3d
Reuven Lerner <reuven@lerner.co.il>
2021-09-09 21:37:38 +0300

    gitautosync update 78

---
+++ flycheck_mymod.py
@@ -0,0 +1,17 @@
+"""This is the first line of the mymod.py docstring.
+
+This is the end of the mymod.py docstring.
+No, it isn't!
+"""
+
+print('Hello from mymod!')
+
+x = 100
+
+y = [10, 20, 30]
+
+
+def hello(name):
+    """Greets a user with a friendly string"""
+    return f'Hello from mymod, {name}!'
+
