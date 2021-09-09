492f9995cb667525cd8440a0be17c2d38bceee82
Reuven Lerner <reuven@lerner.co.il>
2021-09-09 21:31:23 +0300

    gitautosync update 67

--- vowels.py
+++ vowels.py
@@ -4,4 +4,6 @@ def count_vowels(filename):
     for one_line in open(filename):
         for one_character in one_line.lower():
             if one_character in 'aeiou':
-                
+                count += 1
+
+    return count
