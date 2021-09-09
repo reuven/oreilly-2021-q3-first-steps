74e19fca61e0f7c5f4bb77f9c4453eabea542aeb
Reuven Lerner <reuven@lerner.co.il>
2021-09-09 21:19:04 +0300

    gitautosync update 59

--- flycheck_mymod.py
+++
@@ -1,8 +0,0 @@
-x = 100
-
-y = [10, 20, 30]
-
-
-def hello(name):
-    """
-    return f'Hello from mymod, {name}!'
--- mymod.py
+++ mymod.py
@@ -4,4 +4,5 @@ y = [10, 20, 30]
 
 
 def hello(name):
+    """Greets a user with a friendly string"""
     return f'Hello from mymod, {name}!'
